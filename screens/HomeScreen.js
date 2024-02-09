1c96d2cd9df6dcd489e12dfae1a76a3e4fa98e7e
wannabefrontdeveloper <giwonk@naver.com>
2024-02-09 16:10:48 +0900

    코드수정 화면 컴포넌트에서 navigation.setOptions 함수를 사용하여 타이틀 텍스트를 변경하였습니다.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Button} from 'react-native';
 
 function HomeScreen({navigation}) {
+  useEffect(() => {
+    navigation.setOptions({title: '홈'});
+  }, [navigation]);
   return (
     <View>
       <Button
