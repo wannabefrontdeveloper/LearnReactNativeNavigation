6b7d2040c17d438acb97aabb54764abf9eb7fa00
wannabefrontdeveloper <giwonk@naver.com>
2024-02-09 16:01:18 +0900

    코드수정 push 함수를 사용해 화면을 전환하도록 하였습니다.

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
 
-function DetailScreen({route}) {
+function DetailScreen({route, navigation}) {
   return (
     <View style={styles.block}>
       <Text style={styles.text}>id: {route.params.id}</Text>
+      <Button
+        title="다음"
+        onPress={() => navigation.push('Detail', {id: route.params.id + 1})}
+      />
     </View>
   );
 }
