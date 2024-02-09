33be68091e55415c2abeb3332b60668cfd0b2237
wannabefrontdeveloper <giwonk@naver.com>
2024-02-09 16:33:51 +0900

    코드수정 Headerless 화면을 열어주는 버튼을 만들어주었습니다

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -19,6 +19,10 @@ function HomeScreen({navigation}) {
         title="Detail 3 열기"
         onPress={() => navigation.push('Detail', {id: 3})}
       />
+      <Button
+        title="Headerless 열기"
+        onPress={() => navigation.push('Headerless')}
+      />
     </View>
   );
 }
