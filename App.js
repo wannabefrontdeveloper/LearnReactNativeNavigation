dc43d05fc31d561e200da069d687a94f68e28fcc
wannabefrontdeveloper <giwonk@naver.com>
2024-02-11 08:57:02 +0900

    코드수정 네이티브 스택 네비게이터를 설정한 뒤에 MainScreen과 DetailScreen 컴포넌트를 추가하였습니다.

--- App.js
+++ App.js
@@ -1,73 +1,22 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {Text} from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import MainScreen from './screens/MainScreen';
+import DetailScreen from './screens/DetailScreen';
 
-const Tab = createBottomTabNavigator();
-
-function HomeScreen({navigation}) {
-  return <Text>Home</Text>;
-}
-
-function SearchScreen({navigation}) {
-  return <Text>Search</Text>;
-}
-
-function NotificationScreen({navigation}) {
-  return <Text>Notification</Text>;
-}
-
-function MessageScreen({navigation}) {
-  return <Text>Message</Text>;
-}
+const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={({route}) => ({
-          tabBarIcon: ({color, size}) => {
-            let iconName;
-
-            if (route.name === 'Home') {
-              iconName = 'home';
-            } else if (route.name === 'Search') {
-              iconName = 'search';
-            } else if (route.name === 'Notification') {
-              iconName = 'notifications';
-            } else if (route.name === 'Message') {
-              iconName = 'message';
-            }
-
-            return <Icon name={iconName} color={color} size={size} />;
-          },
-          tabBarActiveTintColor: '#fb8c00',
-          tabBarInactiveTintColor: 'gray',
-          tabBarShowLabel: false,
-        })}>
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{title: '홈'}}
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Main"
+          component={MainScreen}
+          options={{headerShown: false}}
         />
-        <Tab.Screen
-          name="Search"
-          component={SearchScreen}
-          options={{title: '검색'}}
-        />
-        <Tab.Screen
-          name="Notification"
-          component={NotificationScreen}
-          options={{title: '알림'}}
-        />
-        <Tab.Screen
-          name="Message"
-          component={MessageScreen}
-          options={{title: '메시지'}}
-        />
-      </Tab.Navigator>
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
