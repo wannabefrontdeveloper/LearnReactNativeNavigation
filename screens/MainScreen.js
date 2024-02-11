132bba69f6082608f3d9de9316a42020b4b39ac4
wannabefrontdeveloper <giwonk@naver.com>
2024-02-11 09:22:06 +0900

    코드수정 머터리얼 상단 탭 네비게이터를 생성하였습니다.

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {Text, View, Button} from 'react-native';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import {Text, View, Button, StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 
 function HomeScreen({navigation}) {
   return (
@@ -21,55 +21,58 @@ function HomeScreen({navigation}) {
   );
 }
 
-function SearchScreen({navigation}) {
+function SearchScreen() {
   return (
-    <View>
-      <Text>Search</Text>;
+    <View style={styles.screenContainer}>
+      <Text>Search</Text>
     </View>
   );
 }
 
-function NotificationScreen({navigation}) {
+function NotificationScreen() {
   return (
-    <View>
-      <Text>Notification</Text>;
+    <View style={styles.screenContainer}>
+      <Text>Notification</Text>
     </View>
   );
 }
 
-function MessageScreen({navigation}) {
+function MessageScreen() {
   return (
-    <View>
-      <Text>Message</Text>;
+    <View style={styles.screenContainer}>
+      <Text>Message</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 function MainScreen() {
   return (
     <Tab.Navigator
       initialRouteName="Home"
       screenOptions={{
-        tabBarActiveTintColor: '#fb8c00',
         tabBarShowLabel: false,
       }}>
       <Tab.Screen
         name="Home"
         component={HomeScreen}
         options={{
-          title: '홈',
-          tabBarIcon: ({color, size}) => (
-            <Icon name="home" color={color} size={size} />
-          ),
+          tabBarIcon: ({color}) => <Icon name="home" color={color} size={24} />,
         }}
       />
       <Tab.Screen
         name="Search"
         component={SearchScreen}
         options={{
-          title: '검색',
-          tabBarIcon: ({color, size}) => (
-            <Icon name="search" color={color} size={size} />
+          tabBarIcon: ({color}) => (
+            <Icon name="search" color={color} size={24} />
           ),
         }}
       />
@@ -77,9 +80,8 @@ function MainScreen() {
         name="Notification"
         component={NotificationScreen}
         options={{
-          title: '알림',
-          tabBarIcon: ({color, size}) => (
-            <Icon name="notifications" color={color} size={size} />
+          tabBarIcon: ({color}) => (
+            <Icon name="notifications" color={color} size={24} />
           ),
         }}
       />
@@ -87,9 +89,8 @@ function MainScreen() {
         name="Message"
         component={MessageScreen}
         options={{
-          title: '메시지',
-          tabBarIcon: ({color, size}) => (
-            <Icon name="message" color={color} size={size} />
+          tabBarIcon: ({color}) => (
+            <Icon name="message" color={color} size={24} />
           ),
         }}
       />
