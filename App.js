c387504c5e2fb0bf0342a67a63ade6492a4aa8a7
wannabefrontdeveloper <giwonk@naver.com>
2024-02-11 10:00:21 +0900

    코드수정 options Props에 일반 객체가 아닌 객체를 반환하는 함수를 넣어주었습니다.

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {
+  getFocusedRouteNameFromRoute,
+  NavigationContainer,
+} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MainScreen from './screens/MainScreen';
 import DetailScreen from './screens/DetailScreen';
 
 const Stack = createNativeStackNavigator();
 
+function getHeaderTitle(route) {
+  const routeName = getFocusedRouteNameFromRoute(route);
+  console.log(routeName);
+  return routeName;
+}
+
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Main" component={MainScreen} />
+        <Stack.Screen
+          name="Main"
+          component={MainScreen}
+          options={({route}) => ({
+            title: getHeaderTitle(route),
+          })}
+        />
         <Stack.Screen name="Detail" component={DetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
