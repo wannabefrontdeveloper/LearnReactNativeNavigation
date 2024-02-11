c5a0f872684c959cbfab5986052cb313494cd4a0
wannabefrontdeveloper <giwonk@naver.com>
2024-02-11 10:10:34 +0900

    코드수정 useNavigation Hook을 사용하여 Detail 1 열기 버튼을 OpenDetailButton이라는 컴포넌트로 따로 분리해서 사용하도록 하였습니다.

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import {Text, View, Button, StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import {useNavigation} from '@react-navigation/native';
 
 const Tab = createMaterialBottomTabNavigator();
 
-function OpenDetailButton({navigation}) {
+function OpenDetailButton() {
+  const navigation = useNavigation();
   return (
     <Button
       title="Detail 1 열기"
@@ -18,7 +20,7 @@ function HomeScreen({navigation}) {
   return (
     <View>
       <Text>Home</Text>
-      <OpenDetailButton navigation={navigation} />
+      <OpenDetailButton />
     </View>
   );
 }
